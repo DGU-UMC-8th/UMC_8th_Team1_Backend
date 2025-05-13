68bc0c2d730e87e63c4a35902b654a6e3002d6be
박지현 <wlgusqkr11@naver.com>
2025-05-13 20:49:37 +0900

    feat: Create All Entity[Team,Role,Tech,User], Enum[Roles]

--- src/main/java/com/team1/changup/domain/Role.java
+++ src/main/java/com/team1/changup/domain/Role.java
@@ -1,5 +1,28 @@
 package com.team1.changup.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
 }
---
+++ src/main/java/com/team1/changup/domain/enums/Roles.java
@@ -0,0 +1,7 @@
+package com.team1.changup.domain.enums;
+
+public enum Roles {
+
+    DEVELOPER, DESIGNER, MARKETER, PLANNER
+
+}
