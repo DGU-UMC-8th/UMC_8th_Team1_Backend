f2b6107cae105e532fa41c9a1612637063b4deb7
wlgusqkr <102128060+wlgusqkr@users.noreply.github.com>
2025-05-13 20:51:15 +0900

    Revert "feat: 엔티티, Enum 생성"

--- src/main/java/com/team1/changup/domain/Role.java
+++ src/main/java/com/team1/changup/domain/Role.java
@@ -1,28 +1,5 @@
 package com.team1.changup.domain;
 
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class Role {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, length = 20)
-    private String name;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "team_id")
-    private Team team;
 
 }
