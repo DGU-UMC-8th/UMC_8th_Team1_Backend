eab3efcbd94921cc72718d4932dcba005c063a83
박지현 <wlgusqkr11@naver.com>
2025-05-13 19:28:25 +0900

    firstCommit

---
+++ src/main/java/com/team1/changup/ChangupApplication.java
@@ -0,0 +1,13 @@
+package com.team1.changup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1/changup/ChangupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.changup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
